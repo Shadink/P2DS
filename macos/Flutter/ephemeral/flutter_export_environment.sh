11c0565ca2a28eaac5dfc70fe476520d74f3c09f
Shadink <116192882+Shadink@users.noreply.github.com>
2024-05-20 22:36:06 +0200

    Clase Abstracta de Mentira parte 1

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\mario\flutterSDK\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\mario\OneDrive\Escritorio\Studies\.MVR - 3º Ingeniería Informática\Segundo Cuatrimestre\Desarrollo de Software\Practicas\P4\P2DS-master"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
